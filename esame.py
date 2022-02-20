18864215a7e8a2d74160d9c5cf197d906103c38f
Matteo Pio Orsi <91544816+OrsiMatteoPio-SM3201227@users.noreply.github.com>
2022-02-20 22:33:50 +0000

    Step 3

--- esame.py
+++ esame.py
@@ -33,41 +33,47 @@ class CSVTimeSeriesFile():
             elements[-1] = elements[-1].strip()
             # If header is not processed, add elements to the list
             if elements[0] != 'date':
+                # Conversion of the second column from string type to numerical type
+                elements[1] = float(elements[1])
                 list.append(elements)
         # File closing
         file.close()
         # Adding elements of the list to the list of list
         listoflist.extend(list)
+        return listoflist
 
-        # Creation of a list, which will have a string column and a numerical column from the previous list of list
-        definitive_listoflist = []
-        # List of list reading, line by line
-        for string_row in listoflist:
-            # Creaton of a list for each line of the previous list of list
-            definitive_row = []
-            # Line reading, element by element and using an index
-            for i, element in enumerate(string_row):
-                # Adding the first element in string type to the list
-                if i == 0:
-                    definitive_row.append(element)
-                # Conversion of the second column from string type to numerical type
-                else:
-                    try:
-                        definitive_row.append(float(element))
-                    except ExamException as e:
-                        print('Conversion error of the value "{}" to numerical type: "{}"'.format(element, e))
-                        break
-                # If lengths are equal, add elements of the list to the list of list
-                if len(definitive_row) == len(string_row):
-                    definitive_listoflist.extend(definitive_row)
-        return definitive_listoflist
 # ==============================
 #            METHODS
 # ==============================
 #          DSMV method
 # ==============================
-# def detect_similar_monthly_variations():
+def detect_similar_monthly_variations(list, years):
+    first_year_monthly_variations = []
+    second_year_monthly_variations = []
+    for i, year in enumerate(years):
+        for couple in list:
+            if str(year) in couple[0]:
+                if i == 0:
+                    first_year_monthly_variations.append(couple[1])
+                else:
+                    second_year_monthly_variations.append(couple[1])
 
+    for number in range(len(first_year_monthly_variations) - 1):
+        first_year_monthly_variations[number] = abs(first_year_monthly_variations[number] - first_year_monthly_variations[number + 1])
+    first_year_monthly_variations = first_year_monthly_variations[0 : - 1]
+
+    for number in range(len(second_year_monthly_variations) - 1):
+        second_year_monthly_variations[number] = abs(second_year_monthly_variations[number] - second_year_monthly_variations[number + 1])
+    second_year_monthly_variations = second_year_monthly_variations[0 : - 1]
+
+    similar_monthly_variations = []
+    for value in range(len(first_year_monthly_variations)):
+        if abs(first_year_monthly_variations[value] - second_year_monthly_variations[value]) <= 2:
+            similar_monthly_variations.append(True)
+        else:
+            similar_monthly_variations.append(False)
+    
+    return similar_monthly_variations
 
 # ==============================
 #            PROGRAM
@@ -80,4 +86,6 @@ time_series = time_series_file.get_data()
 
 print('Dati contenuti nel file: "{}"'.format(time_series))
 
-# detect_similar_monthly_variations(time_series, years)
+result = detect_similar_monthly_variations(time_series, [1949, 1950])
+
+print('Dati contenuti nel file: "{}"'.format(result))
