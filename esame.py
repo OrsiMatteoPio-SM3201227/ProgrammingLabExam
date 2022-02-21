abc31c5dc99bc0184ba8b1ebe22a7e6402023190
Matteo Pio Orsi <91544816+OrsiMatteoPio-SM3201227@users.noreply.github.com>
2022-02-21 19:54:56 +0000

    Step 3.5

--- esame.py
+++ esame.py
@@ -14,6 +14,10 @@ class CSVTimeSeriesFile():
     def __init__(self, name):
         # Creation of the "name" attribute
         self.name = name
+        try:
+            file = open(self.name, 'r')
+        except ExamException('The file does not exist!'):
+            return None
 
     def get_data(self):
         # Creation of two lists: a list and a list of list
@@ -48,31 +52,39 @@ class CSVTimeSeriesFile():
 #          DSMV method
 # ==============================
 def detect_similar_monthly_variations(list, years):
+    # Creation of two lists, one per year
     first_year_monthly_variations = []
     second_year_monthly_variations = []
+    # Loop for each year
     for i, year in enumerate(years):
+        # Loop for each couple in input list
         for couple in list:
+            # If that year is in the couple, add element to the years list
             if str(year) in couple[0]:
+                # If it is processing the first year, add element to the first year list
                 if i == 0:
                     first_year_monthly_variations.append(couple[1])
+                # If it is processing the second year, add element to the second year list
                 else:
                     second_year_monthly_variations.append(couple[1])
-
+    # Calculation of the difference of values ​​for each ordered pair of months of the first year, replacing the elements in the first year list and deleting the last value of 12th month
     for number in range(len(first_year_monthly_variations) - 1):
         first_year_monthly_variations[number] = abs(first_year_monthly_variations[number] - first_year_monthly_variations[number + 1])
     first_year_monthly_variations = first_year_monthly_variations[0 : - 1]
-
+    # Calculation of the difference of values ​​for each ordered pair of months of the second year, replacing the elements in the second year list and deleting the last value of 12th month
     for number in range(len(second_year_monthly_variations) - 1):
         second_year_monthly_variations[number] = abs(second_year_monthly_variations[number] - second_year_monthly_variations[number + 1])
     second_year_monthly_variations = second_year_monthly_variations[0 : - 1]
-
+    # Creation of a list
     similar_monthly_variations = []
+    # Loop for each value in the first year list
     for value in range(len(first_year_monthly_variations)):
+        # If the difference of the differences of the values for each ordered pair of months of the two respective years is ±2, add true to the list
         if abs(first_year_monthly_variations[value] - second_year_monthly_variations[value]) <= 2:
             similar_monthly_variations.append(True)
+        # If the difference of the differences of the values for each ordered pair of months of the two respective years is not ±2, add false to the list
         else:
             similar_monthly_variations.append(False)
-    
     return similar_monthly_variations
 
 # ==============================
@@ -80,7 +92,7 @@ def detect_similar_monthly_variations(list, years):
 # ==============================
 #          Main Program
 # ==============================
-time_series_file = CSVTimeSeriesFile(name = 'data.csv')
+time_series_file = CSVTimeSeriesFile(name = 'dat.csv')
 
 time_series = time_series_file.get_data()
 
